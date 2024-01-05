08b18b37afb559ccc07e979224cfc4f39a943a31
ksmerwin <35506115+ksmerwin@users.noreply.github.com>
2024-01-05 13:13:38 -0600

    Initial Commit

---
+++ Gains/GainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GainsApp.swift
+//  Gains
+//
+//  Created by Developer on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct GainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
