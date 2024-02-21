a3c74e02b2d1068db652658898bbfb2ce21b669f
ksmerwin <35506115+ksmerwin@users.noreply.github.com>
2024-02-21 10:14:04 -0600

    search user and go to profile

---
+++ Gains/Core/TabViews/TabViews/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Gains
+//
+//  Created by Developer on 2/12/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
---
+++ Gains/Core/TabViews/TabViews/Search/SearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewModel.swift
+//  Gains
+//
+//  Created by Developer on 2/21/24.
+//
+
+import Foundation
